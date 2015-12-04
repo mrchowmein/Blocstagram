9d5ee7f21c0fafa3113977105b30eb0580de0a68
Jason Chan <jasonfchan@gmail.com>
2015-12-03 19:06:36 -0800

    checkpoint33

---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  
+//
+//  Created by Jason Chan MBP on 12/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
---
+++ Blocstagram/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  
+//
+//  Created by Jason Chan MBP on 12/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
